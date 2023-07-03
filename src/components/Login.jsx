9e2b7eb2a500c18d5d19cd87a6094e9214baf0b9
IconicLuck <uhasib959@gmail.com>
2023-07-03 18:37:59 +0600

    created an observer to observe the current user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,17 @@ import { Link } from "react-router-dom";
 
 
 const Login = () => {
+
+    const handleLogin = event => {
+        event.preventDefault()
+
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+
+        console.log(email, password)
+    }
+
     return (
         <div className="min-h-screen bg-base-200">
             <div className="w-9/12 mx-auto pt-10">
@@ -12,27 +23,29 @@ const Login = () => {
                     </div>
                     <div className="card w-full shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" placeholder="password" className="input input-bordered" />
-                                <label>
-                                    <Link className="label-text-alt link link-hover text-base font-semibold">Forgot password?</Link>
-                                    <Link to='/register' className="label-text-alt link link-hover block mt-2 text-base font-semibold">New to this site?</Link>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <div className="text-center">
-                                    <input className="btn btn-primary w-1/2" type="submit" value="Login" />
+                            <form onSubmit={handleLogin}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="text" name="email" placeholder="email" className="input input-bordered" />
                                 </div>
-                            </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" placeholder="password" name="password" className="input input-bordered" />
+                                    <label>
+                                        <Link className="label-text-alt link link-hover text-base font-semibold">Forgot password?</Link>
+                                        <Link to='/register' className="label-text-alt link link-hover block mt-2 text-base font-semibold">New to this site?</Link>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <div className="text-center">
+                                        <input className="btn btn-primary w-1/2" type="submit" value="Login" />
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createContext } from "react";
-import { createUserWithEmailAndPassword, getAuth, signOut, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 
@@ -8,6 +8,8 @@ export const AuthContext = createContext(null)
 const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
@@ -23,8 +25,24 @@ const AuthProvider = ({ children }) => {
         })
     }
 
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser)
+            setUser(currentUser)
+        })
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
     const authInfo = {
+        user,
         createUser,
+        signIn,
         logOut,
         updateUsersProfile
     }

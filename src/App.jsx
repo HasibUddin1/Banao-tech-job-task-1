ea229b318aa1341efe763e8a246e76d09d7fcb20
IconicLuck <uhasib959@gmail.com>
2023-07-03 12:45:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <h1 className='mt-2'>
+          Welcome to the website
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
